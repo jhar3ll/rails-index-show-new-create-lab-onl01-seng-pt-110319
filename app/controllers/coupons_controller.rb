3ecceba032c6db4165c1501cb617dd1f1d8eb657
Jharell Chambers <jharell.chambers@selu.edu>
2020-04-08 23:05:04 +0000

    Automatically backed up by Learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -11,7 +11,7 @@ class CouponsController < ApplicationController
   def create
     @coupon = Coupon.new 
     @coupon.coupon_code = params[:coupon_code]
-    @coupon.coupon_store = params[:coupon_store]
+    @coupon.store = params[:store]
     @coupon.save
     redirect_to coupon_path(@coupon)
   end 
