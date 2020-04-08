47f6af4e3a6689f42811dd31e01c0c0851d8197d
Jharell Chambers <jharell.chambers@selu.edu>
2020-04-08 23:13:54 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_coupons.rb
+++ db/migrate/001_create_coupons.rb
@@ -2,7 +2,7 @@ class CreateCoupons < ActiveRecord::Migration
   def change
     create_table :coupons do |t|
       t.string :coupon_code
-      t.string :store 
+      t.string :coupon_store 
       
       t.timestamps null: false 
     end 
